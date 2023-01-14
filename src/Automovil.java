68c8f818ec2f3fd19a749246f5764a8cc37d134d
HoratiuMol <horatiu.moldovan93@gmail.com>
2023-01-14 13:07:41 +0100

    subidas

--- src/Automovil.java
+++ src/Automovil.java
@@ -1,11 +1,78 @@
 public class Automovil{
     //estos son atributos, no métodos
 
-    String fabricante;
-    String modelo;
-    String color = "red";
-    double cilindrada;
+    private String fabricante;
+    private String modelo;
+    private String color = "red";
+    private double cilindrada;
+    private int capacidadEstanque=40;
 
+
+    //distintas formas de implementar, mediante constructor o mediante gettersetter
+
+    public Automovil(String fabricante, String modelo){
+        //no hace nada, tan solo nos permite crear un parámetro vacio
+    }
+
+    public Automovil(String fabricante, String modelo, String color){
+        this(fabricante, modelo);
+        this.color= color;
+    }
+
+    public Automovil(String fabricante, String modelo, String color, double cilindrada) {
+        this.fabricante = fabricante;
+        this.modelo = modelo;
+        this.color = color;
+        this.cilindrada = cilindrada;
+    }
+
+    public Automovil(String fabricante, String modelo, String color, double cilindrada, int capacidadEstanque) {
+        this.fabricante = fabricante;
+        this.modelo = modelo;
+        this.color = color;
+        this.cilindrada = cilindrada;
+        this.capacidadEstanque = capacidadEstanque;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getCilindrada() {
+        return cilindrada;
+    }
+
+    public void setCilindrada(double cilindrada) {
+        this.cilindrada = cilindrada;
+    }
+
+    public int getCapacidadEstanque() {
+        return capacidadEstanque;
+    }
+
+    public void setCapacidadEstanque(int capacidadEstanque) {
+        this.capacidadEstanque = capacidadEstanque;
+    }
 
     public String detalle(){
 //this se usa para atributo de clase no para atributo local
@@ -34,4 +101,19 @@ public class Automovil{
         String frenar=this.frenar();
         return acelerar + "\n" + frenar;
     }
+
+    public float calcularConsumo(int km, float porcentajeBencina){
+
+        return km/(porcentajeBencina*capacidadEstanque);
+
+
+    }
+
+    public float calcularConsumo(int km, int porcentajeBencina){
+
+        return km/((porcentajeBencina/100f)*capacidadEstanque);
+
+    }
+
+    //si se tiene dos metodos llamados iguales pero ocn distintos parametro, se denomino "Sobrecarga de Métodos"
 }
--- src/EjemploAutomovil.java
+++ src/EjemploAutomovil.java
@@ -2,29 +2,28 @@ public class EjemploAutomovil {
 
     public static void main(String[] args) {
 
-        Automovil subaru=new Automovil();
+        Automovil subaru=new Automovil("Subaru", "Impresa");
 
+        subaru.setCilindrada(2.0);
+        subaru.setColor("Blanco");
 
-        subaru.fabricante="Subaru";
-        subaru.modelo="Impresa";
-        subaru.cilindrada=2.0;
-        subaru.color="Blanco";
+        Automovil mazda= new Automovil("Mazda", "BY-50" , "Rojo");
 
+        mazda.setCilindrada(3.0);
+        System.out.println("");
 
-        System.out.println();
+        Automovil nissan=new Automovil("Nissan", "Navarra", "gris oscuro", 3.5, 50 );
 
-        Automovil mazda= new Automovil();
-        mazda.fabricante="Mazda";
-        mazda.modelo="BY-50";
-        mazda.cilindrada=3.0;
-        mazda.color="Rojo";
-
-        System.out.println(subaru.detalle());
-        System.out.println();
-        System.out.println(mazda.detalle());
-        System.out.println();
         System.out.println(subaru.acelerar(3000));
         System.out.println(subaru.frenar());
+        System.out.println(nissan.detalle());
+        System.out.println(mazda.detalle());
+
+        System.out.println("kilometros por litro " + subaru.calcularConsumo(300, 0.6f));
+
+        System.out.println("kilometros por litro " + subaru.calcularConsumo(300, 60));
 
     }
+
+    //principio de Encapsulamiento o de Oculktación (POLIMOFIRSMO)
 }
